f1cda3ea4021e52b8c6bb676864e40ebcf1bcc6b
WhippetsAintDogs <marc.antoine.fortier.94@gmail.com>
2017-02-02 19:59:54 -0500

    Handle more edge cases with install script

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 [[ -d ~/.SUCH-Vim ]] || mkdir ~/.SUCH-Vim
 cp -r ./* ~/.SUCH-Vim
-mv ~/.vimrc ~/.vimrc_old
+if [ -f ~/.vimrc_old ]; then
+	rm ~/.vimrc_old
+fi
+if [ -f ~/.vimrc ]; then
+	cp -Lf ~/.vimrc ~/.vimrc_old
+	rm ~/.vimrc
+fi
 ln -s ~/.SUCH-Vim/vimrc.vim ~/.vimrc
