953e543a8d0896bec1b5db628941acb8cddb9400
nathaniel <nathaniel.simard.1@ulaval.ca>
2017-02-17 20:38:47 -0500

    add pwd for more information

--- run_tests_travis.sh
+++ run_tests_travis.sh
@@ -1,3 +1,4 @@
 ls -la
+pwd
 vim -u ~/.SUCH-Vim/tests/test_vimrc -c "PlugInstall | :q | :q" 
 vim -u ~/.SUCH-Vim/tests/test_vimrc -c "PlugInstall | :q | :Vader! ~/.SUCH-Vim/tests/suchvim/helpers/dependencies.vader" 
