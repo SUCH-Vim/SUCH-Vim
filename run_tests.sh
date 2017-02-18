3829b80ebf0d39dca0baae25eaff488545378b71
nathaniel <nathaniel.simard.1@ulaval.ca>
2017-02-17 21:34:49 -0500

    add nvim for replacing vim

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
-vim -Nu tests/test_vimrc -c "PlugInstall | :q | :q" 
-vim -Nu tests/test_vimrc -c "Vader ~/.SUCH-Vim/tests/suchvim/helpers/dependencies.vader" 
+nvim -Nu tests/test_vimrc -c "PlugInstall | :q | :q" 
+nvim -Nu tests/test_vimrc -c "Vader ~/.SUCH-Vim/tests/suchvim/helpers/dependencies.vader" 
