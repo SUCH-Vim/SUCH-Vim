70fd5cacbd14b5fe3a96efe2491bec51afc38ce8
nathaniel <nathaniel.simard.1@ulaval.ca>
2017-02-17 20:51:50 -0500

    I think it will work

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
-vim -u ~/.SUCH-Vim/tests/test_vimrc -c "PlugInstall | :q | :q" 
-vim -u ~/.SUCH-Vim/tests/test_vimrc -c "PlugInstall | :Vader ~/.SUCH-Vim/tests/suchvim/helpers/dependencies.vader" 
+vim -Nu tests/test_vimrc -c "PlugInstall | :q | :q" 
+vim -Nu tests/test_vimrc -c "Vader ~/.SUCH-Vim/tests/suchvim/helpers/dependencies.vader" 
--- run_tests_travis.sh
+++ run_tests_travis.sh
@@ -1,4 +1,2 @@
-ls -la
-pwd
-vim -u ~/.SUCH-Vim/tests/test_vimrc -c "PlugInstall | :q | :q" 
-vim -u ~/.SUCH-Vim/tests/test_vimrc -c "PlugInstall | :q | :Vader! ~/.SUCH-Vim/tests/suchvim/helpers/dependencies.vader" 
+vim -Nu tests/test_vimrc -c "PlugInstall | :q | :q" 
+vim -Nu tests/test_vimrc -c "Vader! ~/.SUCH-Vim/tests/suchvim/helpers/dependencies.vader" 
