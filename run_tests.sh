320fcdb19d7d7c5e3155f584aa4b9bea316fc4c4
nathaniel <nathaniel.simard.1@ulaval.ca>
2017-02-22 00:18:54 -0500

    fix run tests sh and travis

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 nvim -u ~/.SUCH-Vim/tests/test_vimrc -c "PlugInstall | :q | :q"
-nvim -u ~/.SUCH-Vim/tests/test_vimrc -c "Vader tests/suchvim/helpers/*" 
+nvim -u ~/.SUCH-Vim/tests/test_vimrc -c "Vader tests/unittests/suchvim/dependencies/*" 
